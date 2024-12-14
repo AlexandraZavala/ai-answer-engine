00560e774de9485b7bcd77c23b8573fdb1fef92c
Alexandra Zavala <a20181040@pucp.edu.pe>
2024-12-14 14:35:29 -0500

    clean

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,4 @@
-// TODO: Implement the code here to add rate limiting with Redis by sessions
-// sliding window, rate limit
-//use apikeys in the server
+
 
 // Refer to the Next.js Docs: https://nextjs.org/docs/app/building-your-application/routing/middleware
 // Refer to Redis docs on Rate Limiting: https://upstash.com/docs/redis/sdks/ratelimit-ts/algorithms
